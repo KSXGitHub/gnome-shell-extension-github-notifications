f10b5b4c2491638becbe39dda9b483a74fa70e98
khai96_ <hvksmr1996@gmail.com>
2024-02-21 15:06:02 +0700

    refactor: move buildUri to its own module

--- src/extension.ts
+++ src/extension.ts
@@ -10,34 +10,8 @@ import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js'
 import * as Main from 'resource:///org/gnome/shell/ui/main.js'
 import { MessageTray, SystemNotificationSource, Notification } from 'resource:///org/gnome/shell/ui/messageTray.js'
 
+import buildUri from './buildUri.js'
 import { error } from './log.js'
-
-function buildUri(options: {
-  readonly flags?: GLib.UriFlags
-  readonly scheme: string
-  readonly user?: string | null
-  readonly password?: string | null
-  readonly authParams?: string | null
-  readonly host?: string | null
-  readonly port?: number
-  readonly path?: string
-  readonly query?: string | null
-  readonly fragment?: string | null
-}): GLib.Uri {
-  const {
-    flags = GLib.UriFlags.NONE,
-    scheme,
-    user = null,
-    password = null,
-    authParams = null,
-    host = null,
-    port = -1,
-    path = '',
-    query = null,
-    fragment = null,
-  } = options
-  return GLib.uri_build_with_user(flags, scheme, user, password, authParams, host, port, path, query, fragment)
-}
 
 class GithubNotifications {
   private token: string = ''
