f781863a3612a73d510be697c37cfd56d547b488
khai96_ <hvksmr1996@gmail.com>
2024-02-22 00:29:46 +0700

    fix: type errors

---
+++ .pnpmfile.cjs
@@ -0,0 +1,32 @@
+/**
+ * @param {Record.<String, String>} dependencies
+ */
+function modifyDependencies(dependencies) {
+  for (const name in dependencies) {
+    if (!name.startsWith('@girs/')) continue
+    if (name === '@girs/gjs' || name === '@girs/gnome-shell') continue
+    const [firstChar, ...rest] = dependencies[name]
+    if (firstChar !== '^' && firstChar !== '~') continue
+    dependencies[name] = '=' + rest.join('')
+  }
+}
+
+/**
+ * @param {Object} pkg
+ * @param {String} pkg.name
+ * @param {Record.<String, String>} pkg.dependencies
+ * @param {Record.<String, String>} pkg.devDependencies
+ * @param {Record.<String, String>} pkg.peerDependencies
+ */
+function readPackage(pkg) {
+  if (pkg.name.startsWith('@girs/')) {
+    modifyDependencies(pkg.dependencies)
+    modifyDependencies(pkg.devDependencies)
+    modifyDependencies(pkg.peerDependencies)
+  }
+  return pkg
+}
+
+module.exports = {
+  hooks: { readPackage },
+}
