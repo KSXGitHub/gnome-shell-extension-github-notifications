ed6282f27926f805539612eded84be217d0c6bbb
khai96_ <hvksmr1996@gmail.com>
2024-02-25 01:28:39 +0700

    feat(rust): remove subcommand

--- rust-lib/cli.rs
+++ rust-lib/cli.rs
@@ -2,7 +2,4 @@ use clap::Parser;
 
 #[derive(Debug, Parser)]
 #[clap(about = "Binary helper for the GNOME extension to avoid dealing with libsoup complications")]
-pub enum Cli {
-    #[clap(about = "Fetch notifications from GitHub")]
-    FetchNotifications,
-}
+pub struct Cli;
