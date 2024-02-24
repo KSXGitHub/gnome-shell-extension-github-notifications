582b165d6dcfd0bc61176ab21dc5a5a642a5eb38
khai96_ <hvksmr1996@gmail.com>
2024-02-25 01:21:06 +0700

    refactor(rust): replace writeln with header

--- rust-lib/bindings.rs
+++ rust-lib/bindings.rs
@@ -2,16 +2,19 @@ use crate::{input::Input, output::Output};
 use std::io::Write;
 use typescript_type_def::{write_definition_file, DefinitionFileOptions};
 
-pub fn generate_to<Target>(mut target: Target)
+pub fn generate_to<Target>(target: Target)
 where
     Target: Write,
 {
-    writeln!(target, "// This file was generated, do not edit\n").unwrap();
-    writeln!(target, "// sane-fmt-ignore-file\n").unwrap();
+    let header = concat!(
+        "// This file was generated, do not edit\n",
+        "\n",
+        "// sane-fmt-ignore-file\n",
+    );
     write_definition_file::<_, (Input, Output)>(
         target,
         DefinitionFileOptions {
-            header: None,
+            header: Some(header),
             root_namespace: None,
         },
     )
