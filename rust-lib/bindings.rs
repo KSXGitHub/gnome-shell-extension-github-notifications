f797216258b09f34f1939e48b7711540139ead32
khai96_ <hvksmr1996@gmail.com>
2024-02-25 01:17:24 +0700

    style(bindings): remove namespace `types`

--- rust-lib/bindings.rs
+++ rust-lib/bindings.rs
@@ -1,6 +1,6 @@
 use crate::{input::Input, output::Output};
 use std::io::Write;
-use typescript_type_def::write_definition_file;
+use typescript_type_def::{write_definition_file, DefinitionFileOptions};
 
 pub fn generate_to<Target>(mut target: Target)
 where
@@ -8,6 +8,12 @@ where
 {
     writeln!(target, "// This file was generated, do not edit\n").unwrap();
     writeln!(target, "// sane-fmt-ignore-file\n").unwrap();
-    write_definition_file::<_, (Input, Output)>(target, Default::default())
-        .expect("write TypeScript definitions to output");
+    write_definition_file::<_, (Input, Output)>(
+        target,
+        DefinitionFileOptions {
+            root_namespace: None,
+            ..Default::default()
+        },
+    )
+    .expect("write TypeScript definitions to output");
 }
