7448ed3fc12e5d2097f03e0cc3b1da2ab2bd43e9
khai96_ <hvksmr1996@gmail.com>
2024-02-25 22:07:17 +0700

    perf(rust): remove clap

--- rust-bin/main.rs
+++ rust-bin/main.rs
@@ -1,9 +1,19 @@
-use clap::Parser;
-use gnome_shell_extension_github_notifications::app::App;
-use std::process::ExitCode;
+use gnome_shell_extension_github_notifications::app::{App, ParseError};
+use std::{env::args, process::ExitCode};
 
 fn main() -> ExitCode {
-    if let Err(error) = App::parse().run() {
+    let args: Vec<_> = args().collect();
+
+    let app = match App::parse(&args) {
+        Ok(app) => app,
+        Err(ParseError::EarlyExit(code)) => return code,
+        Err(error) => {
+            eprintln!("ERROR: {error}");
+            return ExitCode::FAILURE;
+        }
+    };
+
+    if let Err(error) = app.run() {
         eprintln!("ERROR: {error}");
         return ExitCode::FAILURE;
     }
--- rust-lib/app.rs
+++ rust-lib/app.rs
@@ -2,17 +2,51 @@ use crate::{
     input::Input,
     output::{Output, ParseResponseError},
 };
-use clap::Parser;
 use derive_more::Display;
 use pipe_trait::Pipe;
-use std::io::stdin;
+use std::{io::stdin, process::ExitCode};
 
-#[derive(Debug, Parser)]
-#[clap(about = "Binary helper for the GNOME extension to avoid dealing with libsoup complications")]
+#[derive(Debug)]
 pub struct App;
 
 #[derive(Debug, Display)]
-pub enum AppError {
+pub enum ParseError<'a> {
+    #[display("Too many CLI arguments")]
+    TooManyArguments,
+    #[display("Unknown command or flag: {_0}")]
+    UnknownArgument(&'a str),
+    #[display("early exit")]
+    EarlyExit(ExitCode),
+}
+
+impl App {
+    pub fn parse<'a, Argument, ArgumentList>(argv: &'a ArgumentList) -> Result<Self, ParseError<'a>>
+    where
+        Argument: AsRef<str> + 'a,
+        ArgumentList: AsRef<[Argument]> + 'a,
+    {
+        if argv.as_ref().len() > 2 {
+            return Err(ParseError::TooManyArguments);
+        }
+
+        match argv.as_ref().get(1).map(AsRef::as_ref) {
+            Some("--help" | "-h" | "help") => {
+                println!("Binary helper for the GitHub Notifications GNOME extension");
+                println!();
+                println!("USAGE: Pipe a JSON object of type Input to stdin of this program and receive result via stdout");
+                println!();
+                println!("FLAGS:");
+                println!("  --help, -h: See this message");
+                ExitCode::SUCCESS.pipe(ParseError::EarlyExit).pipe(Err)
+            }
+            Some(first_arg) => first_arg.pipe(ParseError::UnknownArgument).pipe(Err),
+            None => Ok(App),
+        }
+    }
+}
+
+#[derive(Debug, Display)]
+pub enum RunError {
     #[display("Cannot parse STDIN as JSON: {_0}")]
     ParseJson(serde_json::Error),
     #[display("Failed to fetch notifications: {_0}")]
@@ -23,19 +57,19 @@ pub enum AppError {
 }
 
 impl App {
-    pub fn run(self) -> Result<(), AppError> {
+    pub fn run(self) -> Result<(), RunError> {
         let App = self;
 
         let output = stdin()
             .pipe(serde_json::from_reader::<_, Input>)
-            .map_err(AppError::ParseJson)?
+            .map_err(RunError::ParseJson)?
             .get()
             .send()
-            .map_err(AppError::Request)?
+            .map_err(RunError::Request)?
             .pipe(Output::try_from)
-            .map_err(AppError::ParseResponse)?
+            .map_err(RunError::ParseResponse)?
             .pipe_ref(serde_json::to_string)
-            .map_err(AppError::SerializeResponse)?;
+            .map_err(RunError::SerializeResponse)?;
 
         println!("{output}");
 
